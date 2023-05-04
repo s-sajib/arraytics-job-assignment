1c1861eccce1b0f921b3551b1afd38fd181c202e
Md. Shohidujjaman Sajib <sajibbmekuet@gmail.com>
2023-05-04 22:06:32 +0600

    user, item create ok. culprit found

--- server/models/Item.js
+++ server/models/Item.js
@@ -1,12 +1,9 @@
 const mongoose = require("mongoose");
-const AutoIncrement = require("mongoose-sequence")(mongoose);
 // Item schema
 const itemSchema = new mongoose.Schema({
   name: { type: String, required: true },
   created_at: { type: Date, default: Date.now },
   // created_by: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
 });
-
-// itemSchema.plugin(AutoIncrement, { inc_field: "id" });
 
 module.exports = mongoose.model("Item", itemSchema);
--- server/models/User.js
+++ server/models/User.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose");
-const AutoIncrement = require("mongoose-sequence")(mongoose);
 
 // User schema
 const userSchema = new mongoose.Schema({
-  id: { type: Number, default: 1 },
+  // id: { type: Number, default: 1 },
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
@@ -11,5 +10,5 @@ const userSchema = new mongoose.Schema({
   created_by: { type: Number, default: null, required: false },
 });
 
-userSchema.plugin(AutoIncrement, { inc_field: "id" });
+// userSchema.plugin(AutoIncrement, { inc_field: "id" });
 module.exports = mongoose.model("User", userSchema);
