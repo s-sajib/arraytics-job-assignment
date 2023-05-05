c071225cc51ca7611faed1eee1f4a429d8c1ef66
Md. Shohidujjaman Sajib <sajibbmekuet@gmail.com>
2023-05-05 20:37:19 +0600

    refresh token regenerates accessToken

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -8,6 +8,16 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const verifyToken = require("../middlewares/verifier");
 
+function generateAccessToken(id) {
+  return jwt.sign(
+    {
+      _id: id,
+      exp: Math.floor(Date.now() / 1000) + 15 * 60 * 1000,
+    },
+    process.env.JWT_SECRET_KEY
+  );
+}
+
 // Define routes for authentication
 
 //user registration
@@ -24,12 +34,10 @@ router.post("/register", async (req, res) => {
   // res.send(user);
   try {
     const validation = await user.validate();
-    console.log("User is valid!");
+
     const savedUser = await user.save();
-    console.log("User registered successfully!");
     res.send("User registered successfully!");
   } catch (err) {
-    console.log("Error:", err.message);
     res.status(400).send(err);
   }
 });
@@ -46,8 +54,12 @@ router.post("/login", async (req, res) => {
   if (!validPassword) {
     return res.status(400).send("Email or password is incorrect!");
   }
-  const accessToken = jwt.sign({ _id: user._id }, process.env.JWT_SECRET_KEY);
-  const refreshToken = jwt.sign({ _id: user._id }, process.env.JWT_REFRESH_KEY);
+
+  const accessToken = generateAccessToken(user._id);
+  const refreshToken = jwt.sign(
+    { _id: user._id, exp: Math.floor(Date.now() / 1000) + 8 * 60 * 60 * 1000 }, // token will be expired after 8 hours
+    process.env.JWT_REFRESH_KEY
+  );
   res
     .cookie("access_token", accessToken, {
       httpOnly: true,
@@ -76,6 +88,29 @@ router.get("/user", verifyToken, async (req, res) => {
     return res.status(404).send("User not found!");
   }
   res.send(user);
+});
+
+//refresh accessToken if expired
+router.post("/refresh", async (req, res) => {
+  const refreshToken = req.body.refreshToken;
+  if (!refreshToken) {
+    return res.status(400).send("Refresh token not found!");
+  }
+
+  try {
+    const token = jwt.verify(refreshToken, process.env.JWT_REFRESH_KEY);
+    const accessToken = generateAccessToken(token._id);
+    res
+      .cookie("access_token", accessToken, {
+        httpOnly: true,
+        sameSite: "none", // Set to 'lax' or 'strict' if needed
+        secure: true, // Set to true if using HTTPS
+        expires: new Date(Date.now() + 15 * 60 * 1000), // cookie will be removed after 15 minutes
+      })
+      .send({ message: "Access Token Regenerated successfully!" });
+  } catch {
+    res.status(401).send("Refresh token is invalid!");
+  }
 });
 
 module.exports = router;
